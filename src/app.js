0d4c3f7762de5bfe4c3aa6665704c94bcd100711
Raunak Sethiya <raunak@peoplegrove.com>
2020-04-25 16:20:08 +0530

    install graphql in koa app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import Koa from "koa";
 import Router from "koa-router";
+import mount from "koa-mount";
+import graphqlHTTP from "koa-graphql";
+
+import schema from "./graphql/schema";
 
 const koa = new Koa();
 const app = new Router();
@@ -7,6 +11,16 @@ const app = new Router();
 app.get("/", async (ctx) => {
   ctx.body = "Test App";
 });
+
+app.use(
+  mount(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    })
+  )
+);
 
 koa.use(app.routes());
 const server = koa.listen(3000, () => {
