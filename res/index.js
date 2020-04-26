f2cd179f532c13cbddaf32d2f0ff920baa641943
Raunak Sethiya <raunak@peoplegrove.com>
2020-04-26 13:12:30 +0530

    basic room icon component

--- res/index.js
+++ res/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import RoomIcon from './components/RoomIcon';
+
 class App extends React.Component {
   render() {
-    return <h1>React App</h1>;
+    return (
+      <>
+        <h1>React App</h1>
+        <RoomIcon />
+      </>
+    );
   }
 }
 
