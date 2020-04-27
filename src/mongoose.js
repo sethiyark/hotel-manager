889a14e5898b1602d41f0ad789ad771216eff23e
krhash <krushnasharma24@gmail.com>
2020-04-27 17:55:56 +0530

    Segregate environment configs and add DB config params

--- src/mongoose.js
+++ src/mongoose.js
@@ -7,7 +7,15 @@ const error = chalk.bold.red;
 const disconnected = chalk.bold.yellow;
 const termination = chalk.bold.yellow;
 
-const DB_URL = config.get('db_url');
+let DB_URL = config.get('db_url');
+
+if (!DB_URL && DB_URL.length <= 0) {
+  const DB_HOST = config.get('db_host');
+  const DB_PORT = config.get('db_port');
+  const DB_NAME = config.get('db_name');
+
+  DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+}
 
 module.exports = () => {
   mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
