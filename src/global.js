98b3aa41689d30f5f0cf1d3cdb5e2c1ab054bb7f
Raunak Sethiya <raunak@peoplegrove.com>
2020-05-01 19:09:48 +0530

    create winston logger instance in utils

--- src/global.js
+++ src/global.js
@@ -2,12 +2,12 @@
 import _ from 'lodash';
 import bluebird from 'bluebird';
 import connectDB from './mongoose';
-import log from './logger'; // initializes winston logger
+import getWinstonLogger from './utils/logger';
 
 global.Promise.map = bluebird.map.bind(bluebird);
 global.Promise.reduce = bluebird.reduce.bind(bluebird);
 global._ = _;
-global.log = log;
+global.log = getWinstonLogger();
 
 // Establish DB connection. Should be invoked only once.
 log.info('Initiating database connection');
