72148b6d4a7382a53327e1891a78c8990a0300f6
krhash <krushnasharma24@gmail.com>
2020-05-03 14:06:50 +0530

    Add config properties to room icon

--- res/js/components/RoomIcon/index.js
+++ res/js/components/RoomIcon/index.js
@@ -1,23 +1,45 @@
 import React, { PureComponent } from 'react';
-
+import _ from 'lodash';
 import './styles/RoomIcon.scss';
 
 class RoomIcon extends PureComponent {
   render() {
-    const { size = 200, color = '#f5f5f5', borderColor = '#000' } = this.props;
+    const { size = 200, color = '#f5f5f5', config = {}, roomNo } = this.props;
+
+    const colorMap = {
+      iconBorderColor: config.airConditioned ? '#4386cf' : '#000',
+      iconShadowColor: config.airConditioned ? '#4386cf' : '#000',
+    };
+
     return (
       <span className="room-block" style={{ width: size * 2, height: size }}>
-        <i
-          className="room-block__icon"
-          style={{ backgroundColor: borderColor }}
-        >
-          <i
-            className="room-block__icon__color"
-            style={{ backgroundColor: color }}
+        <span className="shadow" style={{ color: colorMap.iconShadowColor }}>
+          <span
+            className="room-block__icon"
+            style={{ backgroundColor: colorMap.iconBorderColor }}
           >
-            <i className="room-block__icon__door" />
-          </i>
-        </i>
+            <span
+              className="room-block__icon__color"
+              style={{ backgroundColor: color }}
+            >
+              <span className="room-block__icon__door" />
+
+              {config.western ? (
+                <span className="room-block__icon__washroom">W</span>
+              ) : null}
+            </span>
+          </span>
+        </span>
+
+        {!_.isEmpty(config.priorityCleaned) ? (
+          <span className="room-block__priority-cleaning">
+            {config.priorityCleaned}
+          </span>
+        ) : null}
+
+        {!_.isEmpty(roomNo) ? (
+          <span className="room-block_room-number">{roomNo}</span>
+        ) : null}
       </span>
     );
   }
--- res/js/modules/Dashboard/index.js
+++ res/js/modules/Dashboard/index.js
@@ -2,9 +2,16 @@ import React, { PureComponent } from 'react';
 
 import { RoomIcon } from '../../components';
 
+const roomNo = '303';
+const commonRoomConfig = {
+  western: true,
+  airConditioned: true,
+  priorityCleaned: 'P2',
+};
+
 class Dashboard extends PureComponent {
   render() {
-    return <RoomIcon />;
+    return <RoomIcon config={commonRoomConfig} roomNo={roomNo} />;
   }
 }
 
