dbd1f120d36a22d7c4021d6878d1b47bb1d49a60
Raunak Sethiya <raunak@peoplegrove.com>
2020-05-03 14:24:26 +0530

    add rooms grid in dashboard

--- res/js/components/RoomIcon/index.js
+++ res/js/components/RoomIcon/index.js
@@ -1,18 +1,27 @@
 import React, { PureComponent } from 'react';
 import _ from 'lodash';
+import cx from 'classnames';
+
 import './styles/RoomIcon.scss';
 
 class RoomIcon extends PureComponent {
   render() {
-    const { size = 200, color = '#f5f5f5', config = {}, roomNo } = this.props;
+    const {
+      size = 200,
+      color = '#f5f5f5',
+      config = {},
+      roomNo,
+      className: classExtend,
+    } = this.props;
 
+    const className = cx(['room-block', { [classExtend]: !!classExtend }]);
     const colorMap = {
       iconBorderColor: config.airConditioned ? '#4386cf' : '#000',
       iconShadowColor: config.airConditioned ? '#4386cf' : '#000',
     };
 
     return (
-      <span className="room-block" style={{ width: size * 2, height: size }}>
+      <span className={className} style={{ width: size * 2, height: size }}>
         <span className="shadow" style={{ color: colorMap.iconShadowColor }}>
           <span
             className="room-block__icon"
--- res/js/modules/Dashboard/index.js
+++ res/js/modules/Dashboard/index.js
@@ -1,6 +1,10 @@
 import React, { PureComponent } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { map } from 'lodash';
 
 import { RoomIcon } from '../../components';
+
+import './styles/Dashboard.scss';
 
 const roomNo = '303';
 const commonRoomConfig = {
@@ -9,9 +13,47 @@ const commonRoomConfig = {
   priorityCleaned: 'P2',
 };
 
+const config = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19, 10, 101, 102],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29, 20, 201, 202],
+];
+
 class Dashboard extends PureComponent {
+  renderRow = (room, index) => {
+    const className = index % 2 ? 'icon-right' : 'icon-left';
+    return (
+      <Grid.Column key={room}>
+        <RoomIcon
+          className={className}
+          size={60}
+          config={commonRoomConfig}
+          roomNo={roomNo}
+        />
+      </Grid.Column>
+    );
+  };
+
+  renderCol = (rows, index) => (
+    <Grid.Column key={index}>
+      <Grid container centered relaxed columns={2}>
+        {map(rows, this.renderRow)}
+      </Grid>
+    </Grid.Column>
+  );
   render() {
-    return <RoomIcon config={commonRoomConfig} roomNo={roomNo} />;
+    return (
+      <Grid
+        container
+        centered
+        relaxed
+        doubling
+        columns={3}
+        className="dashboard"
+      >
+        {map(config, this.renderCol)}
+      </Grid>
+    );
   }
 }
 
