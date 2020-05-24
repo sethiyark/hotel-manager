4eadf0168adeb0eace08954ba2d7f69c730dcbf2
krhash <krushnasharma24@gmail.com>
2020-05-25 00:10:22 +0530

    Uppy config

--- client/src/modules/CheckIn/index.tsx
+++ client/src/modules/CheckIn/index.tsx
@@ -16,9 +16,16 @@ import {
 import SignatureCanvas from 'react-signature-canvas';
 import { useQuery } from '@apollo/react-hooks';
 import get from 'lodash/get';
+import map from 'lodash/map';
+import { DashboardModal } from '@uppy/react';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
 
 import { FETCH_ROOM } from '../../api';
 import './styles/CheckIn.scss';
+import createUppyInstance from './uppy';
+
+const uppys = () => map(Array(3), createUppyInstance);
 
 const CheckIn = () => {
   const { id } = useParams();
@@ -36,6 +43,9 @@ const CheckIn = () => {
   });
   const [signature, setSignature] = React.useState();
 
+  const [isDashboardOpen, setIsDashboardOpen] = React.useState(-1);
+  const [uppyInstances] = React.useState(uppys());
+
   const setCanvasSize = () => {
     const canvasWrapper = document.getElementById('canvas-wrapper');
     updateCanvasSize({
@@ -48,6 +58,7 @@ const CheckIn = () => {
     window.addEventListener('resize', setCanvasSize);
     return () => {
       window.removeEventListener('resize', setCanvasSize);
+      uppyInstances.forEach((i) => i.close());
     };
   }, [id]);
 
@@ -62,118 +73,157 @@ const CheckIn = () => {
     canvasInit = true;
   }
 
+  const onImageClick = (_id) => {
+    switch (_id) {
+      case 'img_id_front':
+        setIsDashboardOpen(0);
+        break;
+      case 'img_id_back':
+        setIsDashboardOpen(1);
+        break;
+      case 'img_face':
+        setIsDashboardOpen(2);
+        break;
+      default:
+        // eslint-disable-next-line no-console
+        console.error(`Image onClick handler not defined for id: ${_id}`);
+    }
+  };
+
+  const handleModalClose = () => {
+    setIsDashboardOpen(-1);
+  };
+
   return (
-    <Grid as={Segment} container centered relaxed className="checkin">
-      <Grid.Row columns={3} className="header">
-        <Grid.Column>
-          <div className="identification">
-            <Image
-              src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-              circular
-              size="tiny"
-            />
-            <Image
-              src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-              circular
-              size="tiny"
-            />
-          </div>
-          <div className="customer-info">
-            <Input label="Contact" size="mini" />
-            <Input label="Name" size="mini" />
-            <div className="address">
-              <Label>Address</Label>
-              <TextArea />
+    <>
+      <Grid as={Segment} container centered relaxed className="checkin">
+        <Grid.Row columns={3} className="header">
+          <Grid.Column>
+            <div className="identification">
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+                circular
+                size="tiny"
+                onClick={() => onImageClick('img_id_front')}
+              />
+              <Image
+                src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+                circular
+                size="tiny"
+                onClick={() => onImageClick('img_id_back')}
+              />
             </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column textAlign="center" className="customer">
-          <Image
-            src="https://react.semantic-ui.com/images/wireframe/square-image.png"
-            label={{
-              icon: 'camera',
-              corner: 'right',
-              circular: true,
-              size: 'massive',
-            }}
-            circular
-            size="small"
-          />
-          <Label size="medium">
-            Room no.
-            <Label.Detail>{room.displayName}</Label.Detail>
-          </Label>
-        </Grid.Column>
-        <Grid.Column className="time">
-          <Label size="small">
-            Date Check-in
-            <Label.Detail>{today.format('DD/MM/YYYY')}</Label.Detail>
-          </Label>
-          <Label size="small">
-            Time Check-in
-            <Label.Detail>{today.format('HH:MM a')}</Label.Detail>
-          </Label>
-          <Input
-            type="number"
-            label="No. of Person"
-            className="person-count"
-            size="mini"
-          />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row columns={2}>
-        <Grid.Column className="details">
-          <div className="payment">
-            <Input label="Time" icon="pencil" size="large" />
-            <Input label="Rate" icon="pencil" size="large" />
-            <Input
-              label="Toiletries"
-              type="number"
-              icon="pencil"
-              size="large"
-            />
-            <Input
-              action={
-                <Dropdown
-                  button
-                  basic
-                  floating
-                  options={[
-                    { key: 'cash', text: 'Cash', value: 'cash' },
-                    { key: 'online', text: 'Online', value: 'online' },
-                  ]}
-                  defaultValue="cash"
-                />
-              }
-              label="Advance"
-            />
-            <span>
-              <div className="bill">
-                <Label pointing="right">Bill</Label>
-                <Radio toggle />
+            <div className="customer-info">
+              <Input label="Contact" size="mini" />
+              <Input label="Name" size="mini" />
+              <div className="address">
+                <Label>Address</Label>
+                <TextArea />
               </div>
-              <Button primary>Submit</Button>
-            </span>
-          </div>
-        </Grid.Column>
-        <Grid.Column id="canvas-wrapper">
-          <SignatureCanvas
-            ref={(ref) => {
-              signatureRef = ref;
-            }}
-            canvasProps={{
-              width: canvasSize.width,
-              height: canvasSize.height,
-              className: 'sigCanvas',
-            }}
-            onEnd={() => {
-              setSignature(signatureRef.toDataURL());
-            }}
-          />
-          <Label attached="bottom right">Customer Signature</Label>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+            </div>
+          </Grid.Column>
+          <Grid.Column textAlign="center" className="customer">
+            <Image
+              src="https://react.semantic-ui.com/images/wireframe/square-image.png"
+              label={{
+                icon: 'camera',
+                corner: 'right',
+                circular: true,
+                size: 'massive',
+              }}
+              circular
+              size="small"
+              onClick={() => onImageClick('img_face')}
+            />
+            <Label size="medium">
+              Room no.
+              <Label.Detail>{room.displayName}</Label.Detail>
+            </Label>
+          </Grid.Column>
+          <Grid.Column className="time">
+            <Label size="small">
+              Date Check-in
+              <Label.Detail>{today.format('DD/MM/YYYY')}</Label.Detail>
+            </Label>
+            <Label size="small">
+              Time Check-in
+              <Label.Detail>{today.format('HH:MM a')}</Label.Detail>
+            </Label>
+            <Input
+              type="number"
+              label="No. of Person"
+              className="person-count"
+              size="mini"
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column className="details">
+            <div className="payment">
+              <Input label="Time" icon="pencil" size="large" />
+              <Input label="Rate" icon="pencil" size="large" />
+              <Input
+                label="Toiletries"
+                type="number"
+                icon="pencil"
+                size="large"
+              />
+              <Input
+                action={
+                  <Dropdown
+                    button
+                    basic
+                    floating
+                    options={[
+                      { key: 'cash', text: 'Cash', value: 'cash' },
+                      { key: 'online', text: 'Online', value: 'online' },
+                    ]}
+                    defaultValue="cash"
+                  />
+                }
+                label="Advance"
+              />
+              <span>
+                <div className="bill">
+                  <Label pointing="right">Bill</Label>
+                  <Radio toggle />
+                </div>
+                <Button primary>Submit</Button>
+              </span>
+            </div>
+          </Grid.Column>
+          <Grid.Column id="canvas-wrapper">
+            <SignatureCanvas
+              ref={(ref) => {
+                signatureRef = ref;
+              }}
+              canvasProps={{
+                width: canvasSize.width,
+                height: canvasSize.height,
+                className: 'sigCanvas',
+              }}
+              onEnd={() => {
+                setSignature(signatureRef.toDataURL());
+              }}
+            />
+            <Label attached="bottom right">Customer Signature</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      {map(uppyInstances, (uppy, index) => (
+        <DashboardModal
+          key={index}
+          uppy={uppy}
+          plugins={['webcam']}
+          open={index === isDashboardOpen}
+          animateOpenClose
+          onRequestClose={() => handleModalClose()}
+          showLinkToFileUploadResult
+          proudlyDisplayPoweredByUppy={false}
+          hideUploadButton
+        />
+      ))}
+    </>
   );
 };
 
---
+++ client/src/modules/CheckIn/uppy.ts
@@ -0,0 +1,51 @@
+import Uppy from '@uppy/core';
+import WebCam from '@uppy/webcam';
+import XHRUpload from '@uppy/xhr-upload';
+
+function createUppyInstance() {
+  const uppy = Uppy({
+    meta: { type: 'avatar' },
+    restrictions: {
+      allowedFileTypes: ['image/*'],
+      maxNumberOfFiles: 1,
+    },
+    autoProceed: false,
+    allowMultipleUploads: false,
+  });
+
+  uppy.use(WebCam, { id: 'webcam' });
+
+  uppy.on('upload', (data) => {
+    // eslint-disable-next-line no-console
+    console.log(`Starting upload : ${data}`);
+  });
+
+  uppy.on('file-added', (file) => {
+    // eslint-disable-next-line no-console
+    console.log('Added file', file);
+  });
+
+  uppy.on('complete', (result) => {
+    // eslint-disable-next-line no-console
+    console.log('successful files:', result.successful);
+    // eslint-disable-next-line no-console
+    console.log('failed files:', result.failed);
+  });
+
+  uppy.use(XHRUpload, {
+    endpoint: 'https://upload-endpoint.uppy.io/upload',
+    formData: true,
+    fieldName: 'files[]',
+  });
+
+  uppy.on('upload-success', (file, response) => {
+    // eslint-disable-next-line no-console
+    console.log(file);
+    // eslint-disable-next-line no-console
+    console.log(response);
+  });
+
+  return uppy;
+}
+
+export default createUppyInstance;
